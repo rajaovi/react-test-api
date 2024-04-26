126ac68004705563c856826e85bfffe54a059dfe
Raja <orajacse@gmail.com>
2024-04-26 22:39:42 +0530

    Adding Table Component to Photo List

--- src/pages/photos/index.js
+++ src/pages/photos/index.js
@@ -1,12 +1,32 @@
-import Backtohome from "../../component/backHome"
+import { useEffect, useState } from "react";
+import Backtohome from "../../component/backHome";
+import axiosRes from "../../api/axiosRes";
+import Table from "../../component/table";
 
 const Photolist = () => {
-    return ( 
-    <div className="photoList">
-        <Backtohome />
-        <h1>Photolist</h1>
-    </div>
-    )
-}
 
-export default Photolist
+  const [photoListData, setPhotoListData] = useState([]);
+  const tableColumns = ["id", "title", "url", "thumbnailUrl"];
+
+  useEffect(() => {
+    axiosRes(
+      "https://jsonplaceholder.typicode.com/photos",
+      (res) => {
+        const data = res.data.slice(0, 100);
+        setPhotoListData(data);
+      },
+      (err) => {
+        alert("Error With the API");
+      }
+    );
+  }, []);
+
+  return (
+    <div className="photoList">
+      <Backtohome />
+      <Table columns={tableColumns} data={photoListData} />
+    </div>
+  );
+};
+
+export default Photolist;
