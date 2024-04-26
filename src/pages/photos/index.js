1125dd1c18852405f931d7804896c2b9a708ebd3
Raja <orajacse@gmail.com>
2024-04-26 22:27:54 +0530

    Addded Back button to Photo List

--- src/pages/photos/index.js
+++ src/pages/photos/index.js
@@ -1,5 +1,12 @@
+import Backtohome from "../../component/backHome"
+
 const Photolist = () => {
-    return ( <h1>Photolist</h1>)
+    return ( 
+    <div className="photoList">
+        <Backtohome />
+        <h1>Photolist</h1>
+    </div>
+    )
 }
 
 export default Photolist
