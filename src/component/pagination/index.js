45c52397f6fe68faa027a5c9896b20f3ce8fb3f9
Raja <orajacse@gmail.com>
2024-04-27 15:04:17 +0530

    Pagination Functionality

--- src/component/pagination/index.js
+++ src/component/pagination/index.js
@@ -10,6 +10,10 @@ const Pagination = ({totalCount, pageSize, startEndData, activePageNum=1}) => {
     const [activePageNumber, setActivePageNumber] = useState(activePageNum);
     const [disablePrevBtn, setdDisablePrevBtn] = useState(true);
     const [disableNextBtn, setdDisableNextBtn] = useState(false);
+    const [disablePageFirstBtn, setdDisablePageFirstBtn] = useState(true);
+    const [disablePageLastBtn, setDidisablePageLastBtn] = useState(false);
+    const firstItem = paginationNumber[0];
+    const lastItem = paginationNumber[paginationNumber.length-1];
 
     useEffect(()=> {
         let pageNumberArr = [];
@@ -35,7 +39,6 @@ const Pagination = ({totalCount, pageSize, startEndData, activePageNum=1}) => {
     const handlePrev = (e,curNum) => {
         e.preventDefault();
         const changePageNum = curNum - 1;
-        const firstItem = paginationNumber[0];
         changePageNum === firstItem ? setdDisablePrevBtn(true) : setdDisablePrevBtn(false);
         startEndPageNum(changePageNum);
         setdDisableNextBtn(false);
@@ -44,15 +47,25 @@ const Pagination = ({totalCount, pageSize, startEndData, activePageNum=1}) => {
     const handleNext = (e,curNum) => {
         e.preventDefault();
         const changePageNum = curNum + 1;
-        const lastItem = paginationNumber[paginationNumber.length-1];
         changePageNum === lastItem ? setdDisableNextBtn(true) : setdDisableNextBtn(false);
         startEndPageNum(changePageNum);
         setdDisablePrevBtn(false);
     }
 
+    const handleFirstPage = () => {
+        setdDisablePageFirstBtn(true);
+        setDidisablePageLastBtn(false);
+    }
+
+    const handleLastPage = () => {
+        setdDisablePageFirstBtn(false);
+        setDidisablePageLastBtn(true);
+    }
+
+
     return (
         <div className="pagination">
-            <button className="firstLast firstPage"></button>
+            <button onClick={e => handleFirstPage(e)} className={`firstLast firstPage ${disablePageFirstBtn  ? 'disableBtn' : ''}`}></button>
             <button onClick={e => handlePrev(e, activePageNumber)} className={`prevNext prev ${disablePrevBtn ? 'disableBtn' : ''}`}></button>
             <ul className="paginationList">
                 {
@@ -60,7 +73,7 @@ const Pagination = ({totalCount, pageSize, startEndData, activePageNum=1}) => {
                 }
             </ul>
             <button onClick={e => handleNext(e, activePageNumber)} className={`prevNext next ${disableNextBtn ? 'disableBtn' : ''}`}></button>
-            <button className="firstLast lastPage"></button>
+            <button onClick={e => handleLastPage(e)} className={`firstLast lastPage ${disablePageLastBtn  ? 'disableBtn' : ''}`}></button>
         </div>
     )
 }
